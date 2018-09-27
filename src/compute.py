a1aa574f90f4851ff9a835e18b20f4fd3160ee08
Tim Whitaker <tmwhtkr@gmail.com>
2018-09-27 08:51:49 -0600

    Loop through model layers

--- src/compute.py
+++ src/compute.py
@@ -1,12 +1,8 @@
+import copy
 import torch.nn as nn
 import torch.nn.functional as F
 
-def gram_matrix(input):
-	a, b, c, d = input.size()
-	features = input.view(a* b, c*d)
-	G = torch.mm(features, features.t())
-	return G.div(a*b*c*d)
-
+# Compute Modules
 class ContentLoss(nn.Module):
 	def __init__(self, target):
 		super(ContentLoss, self).__init__()
@@ -25,3 +21,52 @@ class StyleLoss(nn.Module):
 		G = gram_matrix(input)
 		self.loss = F.mse_loss(G, self.target)
 		return input
+
+class Normalization(nn.Module):
+	def __init__(self, mean, std):
+		super(Normalization, self).__init__()
+		self.mean = torch.tensor(mean).view(-1, 1, 1)
+		self.std = torch.tensor(std).view(-1, 1, 1)
+
+	def forward(self, image):
+		return (image - self.mean) / self.std
+
+# Compute Functions
+
+def gram_matrix(input):
+	a, b, c, d = input.size()
+	features = input.view(a* b, c*d)
+	G = torch.mm(features, features.t())
+	return G.div(a*b*c*d)
+
+# VGG19 contains a sequence of layers (Conv2d, ReLU, MaxPool2d, ...)
+# We want to create a copy of vgg that includes our loss layers
+
+def model_and_losses(cnn, normalization_mean, normalization_std,
+					 style_image, content_image
+					 style_layers, content_layers):
+
+	# Bootstrap our network
+	cnn = copy.deepcopy(cnn)
+
+	# Keep track of our losses
+	style_losses = []
+	content_losses = []
+
+	# Start by normalizing our image
+	normalization = Normalization(normalization_mean, normalization_std).to(device)
+	model = nn.Sequential(normalization)
+
+	# Loop through our model and keep track of convolutions
+	i = 0
+	for layer in cnn.children():
+		if isinstance(layer, nn.Conv2d):
+			i += 1
+			name = 'conv_{}'.format(i)
+		elif isinstance (layer, nn.ReLU):
+			name = 'relu_{}'.format(i)
+			layer.nn.ReLU(inplace=False)
+		elif isinstance(layer, nn.MaxPool2d):
+			name = 'pool_{}'.format(i)
+		elif isinstance(layer, nn.BatchNorm2d):
+			name = 'bn_{}'.format(i)
--- src/main.py
+++ src/main.py
@@ -27,8 +27,8 @@ unloader = transforms.ToPILImage()
 style_image = utils.load_image(style_path, loader, device)
 content_image = utils.load_image(content_path, loader, device)
 
-plt.figure()
-utils.show_image(style_image, unloader)
+# Load Pretrained VGG and Normalization Tensors
+cnn = models.vgg19(pretrained=True).features.to(device).eval()
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
-plt.figure()
-utils.show_image(content_image, unloader)
