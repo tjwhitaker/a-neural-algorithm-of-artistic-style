2d589180dcdb2694b76c4d3f9aa1949b4282f75b
Tim Whitaker <tmwhtkr@gmail.com>
2018-09-26 21:20:26 -0600

    Add image processing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import utils
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,3 +7,19 @@ import torch.optim as optim
 
 import torchvision.transforms as transforms
 import torchvision.models as models
+
+# Config
+size = 512
+style_path = '../input/picasso.png'
+content_path = '../input/portrait.png'
+
+# Image Transforms
+loader = transforms.Compose([
+	transforms.resize(size),
+	transforms.toTensor()
+])
+
+unloader = transforms.ToPILImage()
+
+style_image = utils.load_image(style_path)
+content_image = utils.load_image(content_path)
---
+++ src/utils.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torch
+import torchvision.transforms as transforms
+
+def load_image(path, loader):
+	image = loader(Image.open(path)).unsqueeze(0)
+	return image.to(device, torch.float)
+
+def show_image(tensor, unloader):
+	image = unloader(tensor.cpu().clone().squeeze(0))
+	plt.imshow(image)
